9a12badba7b9e37a7c27275d4b96de13dcd5a32b
jdanielestrada <jdanielestrada18@gmail.com>
2018-05-10 11:11:09 -0500

    -modal busqueda cotizaciones -ver menu opciones

---
+++ testAngular2/modules/modals/modal.busquedaCotizaciones.controller.js
@@ -0,0 +1,68 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('appRTA')
+        .controller('busquedaCotizaciones', busquedaCotizaciones);
+
+    busquedaCotizaciones.$inject = ['loginService', 'modalService', 'parametrosService', 'configService', 'RTAService', '$scope', '$uibModalInstance', '$timeout'];
+
+    function busquedaCotizaciones(loginService, modalService, parametrosService, configService, RTAService, $scope, $uibModalInstance, $timeout) {
+        var vm = $scope;
+        
+        vm.cancel = cancel;
+        vm.getDetalleCotizacion = getDetalleCotizacion;
+
+        vm.listaCotizacionesUsuario = [];
+        vm.listaDetalleCotizacion = [];
+
+        getCotizacionesByUsusario();
+
+        function getDetalleCotizacion(item) {
+            
+            let csIdCotizacion = item.CS_ID_COTIZACION;
+
+            vm.objectDialog.LoadingDialog("...");
+
+            RTAService.getDetalleCotizacion(csIdCotizacion)
+               .then(function (data) {
+                   vm.objectDialog.HideDialog();
+                   
+                   if (data.data.length > 0 && data.data[0].length > 0) {
+                       vm.listaDetalleCotizacion = data.data[0];
+
+                       $uibModalInstance.close(vm.listaDetalleCotizacion);
+                   } else {
+                       toastr.warning("No se encontró items asociados a la cotización");
+                       vm.listaDetalleCotizacion = [];
+                   }
+
+               });
+        }
+
+        function getCotizacionesByUsusario() {
+
+             vm.objectDialog.LoadingDialog("...");
+
+             RTAService.getCotizacionesByUsusario(loginService.UserData.ID_USUARIO)
+                .then(function (data) {
+                    vm.objectDialog.HideDialog();
+                    
+                    if (data.data.length > 0 && data.data[0].length > 0) {
+                        vm.listaCotizacionesUsuario = data.data[0];
+
+                    } else {
+                        toastr.warning("No se encontró ninguna cotización realizada por el usuario" + loginService.UserData.NOMBRES_USUARIO + ' ' + loginService.UserData.APELLIDOS_USUARIO);
+                        vm.listaCotizacionesUsuario = [];
+                    }
+                });
+        }
+
+        function cancel() {
+            $uibModalInstance.dismiss('cancel');
+        };
+
+        angular.activarBloqueoTAB(true);
+    }
+}());
+
+
