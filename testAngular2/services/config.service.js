22d8d09120ba5b3096d807f77779da086a7324eb
jdanielestrada <jdanielestrada18@gmail.com>
2018-06-13 08:32:13 -0500

    ultimos cambios liberados

--- testAngular2/services/config.service.js
+++ testAngular2/services/config.service.js
@@ -9,26 +9,20 @@
             var puerto    = $location.port();
 
             var urlbase = protocolo + "://" + host + ":" + puerto;
-
-            var pathApiMVC = "";
+            
             var pathApiNode = "";
 
             if (host === "localhost") {
-                //URL para probar local
-                pathApiMVC = "http://localhost:1576";
+                
                 //NODE URL LOCAL
                 pathApiNode = "http://localhost:1600";
 
                 urlbase = 'http://192.168.1.20';
             } else {
                 
-                //pathApiNode = urlbase + "/webapinode_pos";
+                pathApiNode = urlbase + "/WebApiRta";
             }
-
-            //urlbase = "http://192.168.1.43";
-            //pathApiMVC = urlbase + "/TiMeter/WebApiTickets";
-            //pathApiNode = urlbase + "/webapinode_pos";
-
+               
             var configService = {
                 ApiUrls: {
                     
